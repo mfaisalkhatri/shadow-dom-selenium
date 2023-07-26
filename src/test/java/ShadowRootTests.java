76197c793b9c758d938a154c44948130a0c41732
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2023-07-26 08:33:52 +0530

    added tests for finding shadow root element using javascriptexcutor

--- src/test/java/ShadowRootTests.java
+++ src/test/java/ShadowRootTests.java
@@ -1,3 +1,4 @@
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import pages.htmlelements.HomePage;
 
@@ -6,14 +7,33 @@ import static setup.DriverManager.getDriver;
 
 public class ShadowRootTests extends BaseTest {
 
-    @Test
-    public void testShadowRootElement() {
 
+    @BeforeClass
+    public void navigateToWebsite() {
         getDriver().get("https://www.htmlelements.com/demos/menu/shadow-dom/index.htm");
+    }
 
+    @Test
+    public void testFileMenuShadowRootElement() {
         final HomePage homePage = new HomePage();
         assertEquals(homePage.getFileMenuText(), "File");
-        assertEquals(homePage.getNewMenuText(), "New");
+    }
 
+    @Test
+    public void testNewMenuShadowRootElement() {
+        final HomePage homePage = new HomePage();
+        assertEquals(homePage.getNewMenuText(), "New");
+    }
+
+    @Test
+    public void testEditMenuShadowRootElement() {
+        final HomePage homePage = new HomePage();
+        assertEquals(homePage.getEditMenuText(), "Edit");
+    }
+
+    @Test
+    public void testUndoMenuShadowRootElement() {
+        final HomePage homePage = new HomePage();
+        assertEquals(homePage.getUndoMenuText(), "Undo");
     }
 }
