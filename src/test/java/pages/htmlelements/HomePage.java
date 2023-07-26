3fe42f2cad5cf9b69fd830ccf27e0c10665e42bc
Mohammad Faisal Khatri <mohammadfaisalkhatri@gmail.com>
2023-07-26 13:20:50 +0530

    Added jsexecutor example tests (#12)
    * added tests for finding shadow root element using javascriptexcutor
    
    * htmlelement tests refactored

--- src/test/java/pages/htmlelements/HomePage.java
+++ src/test/java/pages/htmlelements/HomePage.java
@@ -1,6 +1,7 @@
 package pages.htmlelements;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebElement;
 
@@ -25,6 +26,31 @@ public class HomePage {
     public String getNewMenuText() {
         openFileMenu();
         return fileMenu().findElement(By.cssSelector(".smart-menu-drop-down div smart-menu-item.smart-element"))
+                .getAttribute("label");
+    }
+
+    public SearchContext expandRootElement(final WebElement element) {
+        return (SearchContext) ((JavascriptExecutor) getDriver()).executeScript(
+                "return arguments[0].shadowRoot", element);
+    }
+
+    public WebElement editMenu() {
+        final WebElement shadowHost = getDriver().findElement(By.cssSelector(".smart-ui-component"));
+        final SearchContext shadowRoot = expandRootElement(shadowHost);
+        return shadowRoot.findElement(By.cssSelector(".smart-element .smart-menu-main-container smart-menu-items-group:nth-child(2)"));
+    }
+
+    public String getEditMenuText() {
+        return editMenu().getAttribute("label");
+    }
+
+    public void openEditMenu() {
+        editMenu().click();
+    }
+
+    public String getUndoMenuText() {
+        openEditMenu();
+        return editMenu().findElement(By.cssSelector(".smart-menu-drop-down div smart-menu-item.smart-element"))
                 .getAttribute("label");
     }
 }
